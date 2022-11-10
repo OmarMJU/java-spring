cb5406f72732081a768bb4b33bff444c6df4921d
Omar M Juarez <omarmju906p@hotmail.com>
2022-11-10 00:29:06 -0600

    Change the http response message.

--- src/main/java/com/omju/springboot/controller/TestController.java
+++ src/main/java/com/omju/springboot/controller/TestController.java
@@ -2,15 +2,22 @@ package com.omju.springboot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 
 @Controller
 public class TestController {
+    @Value("${value.name}")
+    private String name;
+
+    @Value("${value.lastname}")
+    private String lastName;
+
     @RequestMapping
     @ResponseBody
     public ResponseEntity<String> functionResponse() {
-        return new ResponseEntity<>("Hi! Im a response from the controller... with a change... and more changes... and the last change", HttpStatus.OK);
+        return new ResponseEntity<>("Hi! Im a response from the controller... and my creator is " + this.name + " " + this.lastName + " 😎🤓", HttpStatus.OK);
     }
 }
