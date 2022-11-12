f5556bd9a327201620ff11a8cf46265110794008
Omar M Juarez <omarmju906p@hotmail.com>
2022-11-11 23:00:54 -0600

    Data base configuration.

--- src/main/java/com/omju/springboot/config/ConfigurationProperties.java
+++ src/main/java/com/omju/springboot/config/ConfigurationProperties.java
@@ -1,12 +1,14 @@
 package com.omju.springboot.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
 import com.omju.springboot.bean.ConfigPropertiesBeanImpl;
 import com.omju.springboot.bean.ConfigPropertiesBean;
 import org.springframework.context.annotation.Bean;
 import com.omju.springboot.pojo.UserPojo;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableConfigurationProperties(UserPojo.class)
@@ -20,5 +22,16 @@ public class ConfigurationProperties {
     @Bean
     public ConfigPropertiesBean functionConfig() {
         return new ConfigPropertiesBeanImpl(lastName, name);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:testdb");
+        dataSourceBuilder.username("sa");
+        dataSourceBuilder.password("");
+
+        return  dataSourceBuilder.build();
     }
 }
